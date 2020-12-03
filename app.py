096d2ac98d4c0acb1780fa0f7c88acd999ed70b3
zwang199 <zwang199@illinois.edu>
2020-12-03 01:49:47 -0500

    Update app.py
    test save file, render plots

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, request, redirect, send_from_directory, url_for
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for, send_file
 from bokeh.embed import components
 
 from model import clean_data, predict_label, visual
@@ -20,6 +20,29 @@ app.register_blueprint(visual.bp)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print("file and allowed")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("uploaded saved")
+            process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+            #return visual.visualize()
+            p1 = visual.get_tabs()
+            p2 = visual.get_hist()
+            script, div = components(p1)
+            script2, div2 = components(p2)
+            print(script)
+            return render_template('index.html', have_table=True,
+                                   script=script, div=div,
+                                   script2=script2, div2=div2)
     return render_template('index.html', have_table = False,
                            script = None, div = None)
 
@@ -37,6 +60,7 @@ def allowed_file(filename):
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], 'pred_' + filename, as_attachment=True)
+
 
 
 if __name__ == '__main__':
